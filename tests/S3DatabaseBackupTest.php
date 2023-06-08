5efd05ac21afc69587ffba253ef12577c5eb0b67
Jovan <jovan@frismedia.nl>
2023-06-08 16:20:31 +0200

    wip

---
+++ tests/S3DatabaseBackupTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WittyFox\S3\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class S3DatabaseBackupTest extends TestCase
+{
+    public function testBackupDirectoryCreation()
+    {
+       // TODO
+    }
+}
